e1924c523f1f7dc93b1c5f106fdd14fe057bfe63
TheRossDjickso <aguemespoirpro@gmail.com>
2025-11-09 23:21:36 +0000

    humm

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    build: {
-      outDir: "dist",
-        assetsDir: "assets",
-        sourcemap: false,
-        minify: "terser",
-        rollupOptions: {
-          output: {
-              manualChunks: {
-                  vendor: ["react", "react-dom"],
-                  icons: ["lucide-react"]
-              }
-          }
-        }
-    },
-    preview : {
-      port: 3000,
-        open: true
-    }
 })
